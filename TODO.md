01c9085d7830cbcb57ee171a0c0f6f2252d3b367
Daniel Redly <dredly@gmail.com>
2023-01-29 17:38:31 +0000

    Refactoring

--- TODO.md
+++ TODO.md
@@ -1,2 +1,5 @@
 - [x] Figure out pattern for metronome with strong and weak beat
 - [ ] Implement slider for changing bpm
+    - [x] Basic functionality
+    - [ ] Display bpm in text too
+    - [ ] Select an exact bpm
--- src/components/forms/BpmControls.tsx
+++ src/components/forms/BpmControls.tsx
@@ -1,10 +1,16 @@
+import React from "react"
+
 const MIN_BPM = 30
 const MAX_BPM = 300
 
 const BpmControls = ({bpm, updateBpm}: {bpm: number, updateBpm: (arg0: number) => void}) => {
+    const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+        updateBpm(Number(evt.target.value))
+    }
+
     return (
         <form onSubmit={evt => { evt.preventDefault() }}>
-            <input key="changebpm" type="range" min={MIN_BPM} max={MAX_BPM} value={bpm} onChange={evt => {updateBpm(Number(evt.target.value))}} />
+            <input key="changebpm" type="range" min={MIN_BPM} max={MAX_BPM} value={bpm} onChange={handleChange} />
         </form>
     ) 
 }
